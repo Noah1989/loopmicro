874b5be98d2b15539d061bf4906c18b5c81a3235
Stefan Paul Noack <stefan@noack.software>
2020-01-16 23:36:06 +0100

    minor

--- rom/extract_symbols.sh
+++ rom/extract_symbols.sh
@@ -2,5 +2,5 @@
 
 (for label in $(grep -oE 'entrypoint.+$' $1.lst | cut -d' ' -f2); \
 do grep -iE "^${label}:" rom.lst; done) \
-| sed -e "s/^/${1^^}_/" -e 's/:\s*/ .equ $/;s/ DEFINED AT LINE.*//' \
+| sed -e "s/^/${1^^}_/" -e 's/:\s*/ .EQU $/;s/ DEFINED AT LINE.*//' \
 > $1.inc
