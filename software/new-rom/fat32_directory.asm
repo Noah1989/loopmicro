c8ed60ac5f4bd47c2c49d96012cbb2fc5b0a296b
Stefan Paul Noack <stefan@noack.software>
2020-12-26 16:24:55 +0100

    decode some of the ucs-2 characters in filenames, improve short file names

--- software/new-rom/fat32_directory.asm
+++ software/new-rom/fat32_directory.asm
@@ -107,20 +107,44 @@ fat32_directory_listing_IX_read_line_handle_regular_entry:
 	LD	A, (IX+listing_lfn_sequence)
 	CP	A, 1 ; validate lfn sequence
 	JR	Z, fat32_directory_listing_IX_read_line_ok
-	; use short file name
+	; if lfn invalid, use short file name
 	LD	C, (IX+listing_buffer_size)
-	DEC	C ; null byte at end
-	LD	A, C
-	CP	A, 11 ; check buffer size
-	JR	C, fat32_directory_listing_IX_read_line_copy_short_name
-	LD	C, 11
-fat32_directory_listing_IX_read_line_copy_short_name:
 	LD	E, (IX+listing_buffer_address)
 	LD	D, (IX+listing_buffer_address+1)
-	LD	B, 0
-	LDIR	; copy BC bytes from (HL) to (DE)
-	XOR	A, A
-	LD	(DE), A ; terminator
+	LD	B, 8
+fat32_directory_listing_IX_read_line_copy_short_filename_loop:
+	LD	A, (HL)
+	INC	HL
+	CP	A, ' '
+	JR	Z, fat32_directory_listing_IX_read_line_copy_short_filename_skip_space
+	LD	(DE), A
+	DEC	C
+	JR	Z, fat32_directory_listing_IX_read_line_copy_short_filename_write_terminator
+	INC	DE
+fat32_directory_listing_IX_read_line_copy_short_filename_skip_space:
+	DJNZ	fat32_directory_listing_IX_read_line_copy_short_filename_loop
+	; add the dot
+	LD	A, '.'
+	LD	(DE), A
+	DEC	C
+	JR	Z, fat32_directory_listing_IX_read_line_copy_short_filename_write_terminator
+	INC	DE
+	; short extension
+	LD	B, 3
+fat32_directory_listing_IX_read_line_copy_short_extension_loop:
+	LD	A, (HL)
+	INC	HL
+	CP	A, ' '
+	JR	Z, fat32_directory_listing_IX_read_line_copy_short_extension_skip_space
+	LD	(DE), A
+	DEC	C
+	JR	Z, fat32_directory_listing_IX_read_line_copy_short_filename_write_terminator
+	INC	DE
+fat32_directory_listing_IX_read_line_copy_short_extension_skip_space:
+	DJNZ	fat32_directory_listing_IX_read_line_copy_short_extension_loop
+fat32_directory_listing_IX_read_line_copy_short_filename_write_terminator:
+	LD	A, 0
+	LD	(DE), A
 fat32_directory_listing_IX_read_line_ok:
 	INC	A ; clear Z flag
 	RET
@@ -181,6 +205,19 @@ fat32_directory_listing_IX_read_line_handle_lfn_end_write_terminator:
 
 fat32_directory_listing_IX_read_line_handle_lfn_copy_loop:
 	LD	A, (HL)
+	CP	A, $A0
+	JR	C, fat32_directory_listing_IX_read_line_handle_lfn_copy_skip
+	PUSH	HL
+	PUSH	BC
+	SUB	A, $A0
+	LD	B, 0
+	LD	C, A
+	LD	HL, ucs2_map_dirty
+	ADD	HL, BC
+	LD	A, (HL)
+	POP	BC
+	POP	HL
+fat32_directory_listing_IX_read_line_handle_lfn_copy_skip:
 	LD	(DE), A
 	DEC	C ; remaining buffer
 	RET	Z ;
@@ -190,3 +227,12 @@ fat32_directory_listing_IX_read_line_handle_lfn_copy_loop:
 	DJNZ	fat32_directory_listing_IX_read_line_handle_lfn_copy_loop
 	INC	B ; clear Z flag
 	RET
+
+ucs2_map_dirty: ; start at code point $00A0
+defb	255, 173, 155, 156, '?', 157, '?', 21, '?', '?', 166, 174, 170, '?'
+defb	'?', '?', 248, 241, 253, '?', '?', 230, 20, 250, '?', '?', 167, 175
+defb	172, 171, '?', 168, '?', '?', '?', '?', 142, 143, 146, 128, '?', 144
+defb	'?', '?', '?', '?', '?', '?', '?', 165, '?', '?', '?', '?', 153, '?'
+defb	'?', '?', '?', '?', 154, '?', '?', 225, 133, 160, 131, '?', 132, 134
+defb	145, 135, 138, 130, 136, 137, 141, 161, 140, 139, '?', 164, 149, 162
+defb	147, '?', 148, 246, '?', 151, 163, 150, 129, '?', '?', 152
