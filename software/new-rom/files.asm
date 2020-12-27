26deda7ccaca600ebffa73cca9ba4d3ff4c92deb
Stefan Paul Noack <stefan@noack.software>
2020-12-27 05:45:53 +0100

    ui layout

--- software/new-rom/files.asm
+++ software/new-rom/files.asm
@@ -3,6 +3,8 @@ public files_app
 extern ui_window_handle_input_propagate
 extern ui_window_handle_vsync_noop
 extern ui_listview_IX_draw
+extern ui_panel_IX_draw
+extern ui_label_IX_draw
 
 extern sdcard_init
 extern fat32_init
@@ -17,6 +19,9 @@ include "ui.inc"
 files_app_init:
 	CALL	sdcard_init
 	CALL	fat32_init
+	LD	L, '/'
+	LD	H, 0
+	LD	(files_listview_title_text_name), HL
 	RET
 
 files_app_activate:
@@ -41,7 +46,11 @@ defb	0, 1, 80, 28
 defb	$1F, ' '
 defw	ui_window_handle_input_propagate
 defw	ui_window_handle_vsync_noop
+defw	files_listview_title_panel
+defw	files_listview_title_label
+defw	files_listview_scrollbar
 defw	files_listview
+;defw	files_listview_cursor
 defw	0
 
 files_menu_window:
@@ -62,16 +71,44 @@ defw	files_listview+ui_listview_line_buffer
 defw	fat32_rootdir
 defs	1 ; lfn sequence number
 
+files_listview_title_panel:
+defb	ui_object_type_widget
+defb	0, -2, 38, 1
+defw	files_listview
+defw	ui_panel_IX_draw
+defb	$1E, ' '
+
+files_listview_title_label:
+defb	ui_object_type_widget
+defb	0, 0, 38, 1
+defw	files_listview_title_panel
+defw	ui_label_IX_draw
+defw	files_listview_title_text
+
 files_listview:
 defb	ui_object_type_widget
-defb	1, 0, 38, 28 ; left, top, width, height
+defb	1, 3, 38, 25 ; left, top, width, height
 defw	files_main_window ; ui_widget_parent
 defw	ui_listview_IX_draw ; ui_widget_draw
 defw	files_listing
-defs	39 	; ui_listview_line_buffer
+defs	39 ; ui_listview_line_buffer
 
-files_cursor:
+files_listview_cursor:
 defb	ui_object_type_widget
-defb	0, 0, 38, 28
+defb	0, 0, 38, 25
 defw	files_listview
+defw	error
 defs	0 ; current line
+
+files_listview_scrollbar:
+defb	ui_object_type_widget
+defb	38, -3, 1, 28
+defw	files_listview
+defw	ui_panel_IX_draw
+defb	$19, $B3
+
+section ram_initialized
+files_listview_title_text:
+defb	"Contents of directory "
+files_listview_title_text_name:
+defb	"1234567890123456", 0
